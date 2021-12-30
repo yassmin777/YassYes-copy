ac4e0c012926e7f2ce0e4cc4d369ffec9f17d3b1
Med Jamel Bouassida <74288509+yassine-der@users.noreply.github.com>
2021-12-30 15:46:28 +0100

    auto 2

--- YassYes/Views/matchsListeViewController.swift
+++ YassYes/Views/matchsListeViewController.swift
@@ -42,19 +42,20 @@
                     self.equipe_nomA.removeAll()
                     self.equipe_imageA.removeAll()
                     self.equipeDescriptionA.removeAll()
-print(myresult)
-                    for singleLeagueJson in myresult!.arrayValue {
-                        //ligues.append(makeItem(makeItem(equipes_ids: singleLeagueJson.1)))
-                   print(singleLeagueJson)
+                    for singleLeagueJson in myresult!["equipe_A_id"] {
+                        //ligues.append(makeItem(makeItem(jsonItem: singleLeagueJson.1)))
+                        print(singleLeagueJson.1)
+                   // }
                     //for i in myresult!.arrayValue{
-                        let idLa = singleLeagueJson["_id"].stringValue
-                        let noma = singleLeagueJson["nom"].stringValue
-                        let Descriptiona = singleLeagueJson["discription"].stringValue
-                        let imagea = "http://localhost:3000/"+singleLeagueJson["image"].stringValue
-                        self.equipe_idA.append(idLa)
-                        self.equipe_nomA.append(noma)
-                        self.equipe_imageA.append(imagea)
-                        self.equipeDescriptionA.append(Descriptiona)
+                        let idL = singleLeagueJson.1["_id"].stringValue
+                        let nom = singleLeagueJson.1["nom"].stringValue
+                        let Description = singleLeagueJson.1["discription"].stringValue
+                        let image = "http://localhost:3000/"+singleLeagueJson.1["image"].stringValue
+                        self.equipe_idA.append(idL)
+                        self.equipe_nomA.append(nom)
+                        self.equipe_imageA.append(image)
+                        self.equipeDescriptionA.append(Description)
+
 
 
 
@@ -80,30 +81,28 @@ print(myresult)
                    self.equipe_nomB.removeAll()
                    self.equipe_imageB.removeAll()
                     self.equipeDescriptionB.removeAll()
-                    for singleLeagueJson in myresult!.arrayValue {
-                        //ligues.append(makeItem(makeItem(equipes_ids: singleLeagueJson.1)))
-                        print(singleLeagueJson)
+                    for singleLeagueJson in myresult!["equipe_B_id"] {
+                        //ligues.append(makeItem(makeItem(jsonItem: singleLeagueJson.1)))
+                        print(singleLeagueJson.1)
                    // }
-                    for i in singleLeagueJson["equipe_A_id"]{
-                        let idL = i.1["_id"].stringValue
-                        let nom = i.1["nom"].stringValue
-                        let Description = i.1["discription"].stringValue
-                        let image = "http://localhost:3000/"+i.1["image"].stringValue
+                    //for i in myresult!.arrayValue{
+                        let idL = singleLeagueJson.1["_id"].stringValue
+                        let nom = singleLeagueJson.1["nom"].stringValue
+                        let Description = singleLeagueJson.1["discription"].stringValue
+                        let image = "http://localhost:3000/"+singleLeagueJson.1["image"].stringValue
                         self.equipe_idB.append(idL)
                         self.equipe_nomB.append(nom)
                         self.equipe_imageB.append(image)
                         self.equipeDescriptionB.append(Description)
 
 
-                    }
+
+                    
 
 
                     }
                     self.matchTv.reloadData()
                     break
-
-
-
                 case .failure:
                     print(response.error!)
                     break
@@ -124,20 +123,21 @@ print(myresult)
             let equipeImageA = tv.viewWithTag(2) as! UIImageView
             let equipe_NameB = tv.viewWithTag(3) as! UILabel
             let equipeImageB = tv.viewWithTag(4) as! UIImageView
-            print(equipe_NameA)
+            print(equipe_nomA.count)
+            print(equipe_nomB.count)
             equipe_NameA.text = equipe_nomA[indexPath.row]
             var path = String(equipe_imageA[indexPath.row]).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
                    path = path.replacingOccurrences(of: "%5C", with: "/", options: NSString.CompareOptions.literal, range: nil)
                     let url = URL(string: path)!
                     print(url)
             equipeImageA.af.setImage(withURL: url)
-//            equipe_NameB.text = equipe_nomB[indexPath.row]
-//            var pathB = String(equipe_imageB[indexPath.row]).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-//                   path = pathB.replacingOccurrences(of: "%5C", with: "/", options: NSString.CompareOptions.literal, range: nil)
-//                    let urlB = URL(string: pathB)!
-//                    print(url)
-//            equipeImageB.af.setImage(withURL: urlB)
-                return cell
+            equipe_NameB.text = equipe_nomB[indexPath.row]
+            let pathB = String(equipe_imageB[indexPath.row]).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                   path = pathB.replacingOccurrences(of: "%5C", with: "/", options: NSString.CompareOptions.literal, range: nil)
+                    let urlB = URL(string: pathB)!
+                    print(url)
+            equipeImageB.af.setImage(withURL: urlB)
+                  return cell
         }
      
 func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
